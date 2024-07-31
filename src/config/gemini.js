a15bea3d939ee8368ddbf56a974c3adf6d2deb2c
Cameron <cameron.cw@outlook.com>
2024-08-01 00:13:18 +0100

    Updated API

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -13,7 +13,7 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = "AIzaSyAmavyNkldGAa_0KXX4R92MQ1-uPonEVVc";
+  const apiKey = import.meta.env.VITE_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
